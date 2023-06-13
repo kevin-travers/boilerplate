708cbf37fef7fe1b1f8e289bfa2298d0da621c24
kevin-travers <kevin.a.travers@gmail.com>
2023-06-13 08:13:13 -0400

    docker for elastic search added

--- backend/service/models/jobRequest.go
+++ backend/service/models/jobRequest.go
@@ -15,7 +15,24 @@ type JobRequest struct {
 	JobCreatedDate string `json:"JobCreatedDate"`
 }
 
+type JobRequestFilter struct {
+	JobId          string `json:"JobId"`
+	JobName        string `json:"JobName"`
+	JobType        string `json:"JobType"`
+	JobStatus      string `json:"JobStatus"`
+	JobPriority    string `json:"JobPriority"`
+	JobDescription string `json:"JobDescription"`
+	JobCreatedDate string `json:"JobCreatedDate"`
+	Offset         int    `json:"Offset"`
+	Limit          int    `json:"Limit"`
+}
+
 func (job *JobRequest) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(job)
+}
+
+func (job *JobRequestFilter) FromJSON(r io.Reader) error {
 	e := json.NewDecoder(r)
 	return e.Decode(job)
 }
